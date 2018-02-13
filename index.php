fa88de7aef12089968b1d0bf698a01ee86b2422a
faridg <farid.gassanov@gmail.com>
2018-02-13 16:35:26 -0500

    some shuffling

--- index.php
+++ index.php
@@ -1,43 +1,32 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-
-  <!-- Basic Page Needs
-  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
-  <meta charset="utf-8">
-  <title>Domain Ownership Lookup</title>
-  <meta name="description" content="whois">
-  <meta name="author" content="fargas">
-
-  <!-- Mobile Specific Metas
-  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-
-  <!-- FONT
-  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
-  <link href="https://fonts.googleapis.com/css?family=Noto+Sans" rel="stylesheet" type="text/css">
-
-  <!-- CSS
-  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
-  <link rel="stylesheet" href="css/normalize.css">
-  <link rel="stylesheet" href="css/skeleton.css">
-  <link rel="stylesheet" href="css/custom.css">
-
-  <!-- Favicon
-  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
-  <link rel="icon" type="image/ico" href="images/favicon.ico">
-
+	<meta charset="utf-8">
+	<title>Domain Ownership Lookup</title>
+	<meta name="description" content="whois">
+	<meta name="author" content="i'm faridg on github">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link href="https://fonts.googleapis.com/css?family=Noto+Sans" rel="stylesheet" type="text/css">
+	<link rel="stylesheet" href="css/normalize.css">
+	<link rel="stylesheet" href="css/skeleton.css">
+	<link rel="stylesheet" href="css/custom.css">
+	<link rel="icon" type="image/ico" href="images/favicon.ico">
 </head>
 <body class="code-snippets-visible>
-
 <div class="container">
-<section class="header">
-<h4 class="title">Domain Name Lookup</h4>
-<form action="lookup.php" method="POST">
-<input type="text" name="domain" autofocus="autofocus">
-<input class="button button-primary" type="submit" value="WHOIS">
-</form>
-</section>
+	<div class="row">
+		<section class="header">
+		<h4 class="title">Domain Name Lookup</h4>
+		<form action="lookup.php" method="POST">
+		<input type="text" name="domain" autofocus="autofocus">
+		<input class="button button-primary" type="submit" value="WHOIS">
+		</form>
+		</section>
+	</div>
 </div>
+<footer>
+	<a href="https://github.com/faridg/whoiser" target="_blank">
+	<img src="images/git.png" alt="faridg on github" height="42px" width="42px"></a>
+</footer>
 </body>
 </html>
