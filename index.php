5c4fbd57e01f04b7da8393a3e535da5d56cea158
faridg <farid.gassanov@gmail.com>
2018-02-13 17:12:07 -0500

    corner ribbon

--- index.php
+++ index.php
@@ -4,12 +4,13 @@
 	<meta charset="utf-8">
 	<title>Domain Ownership Lookup</title>
 	<meta name="description" content="whois">
-	<meta name="author" content="i'm faridg on github">
+	<meta name="author" content="faridg on github">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link href="https://fonts.googleapis.com/css?family=Noto+Sans" rel="stylesheet" type="text/css">
 	<link rel="stylesheet" href="css/normalize.css">
 	<link rel="stylesheet" href="css/skeleton.css">
 	<link rel="stylesheet" href="css/custom.css">
+	<link rel="stylesheet" href="css/ribbon.css">
 	<link rel="icon" type="image/ico" href="images/favicon.ico">
 </head>
 <body class="code-snippets-visible>
@@ -25,8 +26,7 @@
 	</div>
 </div>
 <footer>
-	<a href="https://github.com/faridg/whoiser" target="_blank">
-	<img src="images/git.png" alt="faridg on github" height="42px" width="42px"></a>
+<a href="https://github.com/faridg/whoiser" target="_blank"><div class="corner-ribbon bottom-right sticky white"><img src="images/git.png" alt="faridg on github" height="30px" width="30px"></a></div>
 </footer>
 </body>
 </html>
